01d2b30dfd85f7a7e401ba183176f1cadec8124c
Brett Hardin <bhardin@kissmetrics.com>
2013-05-16 07:45:34 -0700

    Adding some greedy algo

--- greedy_epsilon.rb
+++ greedy_epsilon.rb
@@ -1,43 +1,75 @@
 class BernoulliArm
-@probability = nil
+	@probability = nil
 
-def initialize(probability)
-	@probability = probability
-end
+	def initialize(probability)
+		@probability = probability
+	end
 
-def draw
-	if rand > @probability
-		return 0.0 
-	else
-		return 1.0
+	def draw
+		if rand > @probability
+			return 0.0 
+		else
+			return 1.0
+		end
 	end
 end
+
+class EpsilonGreedy
+	@epsilon = @counts = @values = nil
+
+	def initialize(epsilon, counts, values)
+		@epsilon = epsilon
+		@counts = counts
+		@values = values
+	end	
+
+	def update(chosen_arm, reward)
+ #    @counts[chosen_arm] = chosen_arm
+ #    n = @counts[chosen_arm]
+
+ #    value = values[chosen_arm]
+ #    new_value = ((n - 1) / float(n)) * value + (1 / float(n)) * reward
+ #    values[chosen_arm] = new_value
+  end
+
+  def select_arm
+ #    if rand > @epsilon
+ #      return @values.find_index(@values.max)
+ #    else
+ #      return rand(@values.length)
+  end
+
 end
 
-means = [0.2, 0.1, 0.6, 0.1, 0.1]
+means = [0.9, 0.1, 0.1, 0.1]
 means.shuffle!
 
 arms = means.collect { |x|
 BernoulliArm.new(x)
 } 
 
+# This is the bandit_algorithm that will be tested.
 algo = Proc.new {
+	eg = EpsilonGreedy.new(0.1, Array.new, Array.new)
+
 	public
 	def update(chosen_arm, reward)
-		# puts "#{chosen_arm} - #{reward}"
+		#eg.update(chosen_arm, reward)
 	end
 
 	def select_arm
-		1
+		return 1
+		# eg.select_arm
 	end
 }
 
 # Testing framework for testing bandit algorithms
 # 
-# param &algo - Block of code you want to test
-# param [arms] Array - Arms we want to simulate draws from
-# param number_of_simulations - self explanatory.
-# param horizon - The number of times each algorithm is allowed to pull on arms during each simulation.
+# @param algo [Proc] - Block of code you want to test
+# @param arms [Array] - Arms we want to simulate draws from
+# @param number_of_simulations [Fixnum] - self explanatory.
+# @param horizon [Fixnum] - The number of times each algorithm is allowed to pull on arms during each simulation.
+# @return [Array, Array, Array, Array, Array] sim_nums, times, chosen_arms, rewards, cumulative_rewards
 def test_algorithm(algo, arms, number_of_simulations, horizon)
 	# array of arrays
 
@@ -83,4 +115,4 @@ def test_algorithm(algo, arms, number_of_simulations, horizon)
 	return [sim_nums, times, chosen_arms, rewards, cumulative_rewards]
 end
 
-puts test_algorithm(algo, arms, 1, 200000).inspect
+puts test_algorithm(algo, arms, 10, 200).inspect
